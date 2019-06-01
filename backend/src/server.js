0b00a272ef14abafd8f99cc392358f46d081134f
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-06-01 14:34:55 -0500

    setup Node.js/server. integrated MongoDB/Mongoose

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { userRoutes } from './routes/index';
+import { PORT, NODE_ENV, MONGO_URI } from './config';
+
+(async () => {
+  try {
+    await mongoose.connect(MONGO_URI, { userNewUrlParser: true });
+    console.log('MongoDB connected');
+
+const app = express();
+app.disable('x-powered-by');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
+apiRouter.use('/users', userRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (err) {
+    console.log(err)
+  }
+})();

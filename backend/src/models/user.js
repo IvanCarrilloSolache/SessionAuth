1644fdd582b7e9b4e6bdcda80510114e843b2e24
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-06-01 15:39:03 -0500

    implemented models/validation 2st part

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -27,3 +27,14 @@ UserSchema.pre('save', function () {
     this.password = hashSync(this.password, 10);
   }
 });
+
+UserSchema.statics.doesNotExist = async function (field) {
+  return await this.where(field).countDocuments() === 0;
+};
+UserSchema.methods.comparePasswords = function (password) {
+  return compareSync(password, this,password);
+};
+
+const User = mongoose.model('user', UserSchema);
+
+export default User;

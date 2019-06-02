1977b8d1a95b0fbd6ab4457fce6e08a57c3f1745
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-06-02 14:56:23 -0500

    entered connect-mongo to allow connection to be re-established if server shutsdown

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,9 +11,27 @@ import { PORT, NODE_ENV, MONGO_URI, SESS_NAME, SESS_SECRET, SESS_LIFETIME } from
     console.log('MongoDB connected');
 
 const app = express();
+const MongoStore = connectStore(session);
+
 app.disable('x-powered-by');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session({
+  name: SESS_NAME,
+  secret: SESS_SECRET,
+  saveUninitialized: false,
+  resave: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    collection: 'session',
+    ttl: parseInt(SESS_LIFETIME) / 1000
+  }),
+  cookie: {
+    sameSite: true,
+    secure: NODE_ENV === 'production',
+    maxAge: parseInt(SESS_LIFETIME)
+  }
+}));
 
 const apiRouter = express.Router();
 app.use('/api', apiRouter);

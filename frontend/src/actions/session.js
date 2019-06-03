5fa4f36c48f8d4e6db9f7463955836c19218d18d
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-06-02 23:38:52 -0500

    integrating Redux-action creators defined

--- frontend/src/actions/session.js
+++ frontend/src/actions/session.js
@@ -1,8 +1,9 @@
 import * as apiUtil from '..util/session';
-
+// action types
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 
+// actions
 const receiveCurrentUser = user => ({
     type:RECEIVE_CURRENT_USER,
     user
@@ -11,3 +12,33 @@ const receiveCurrentUser = user => ({
 const logoutCurrentUser = () => ({
     type:LOGOUT_CURRENT_USER
 });
+// action creators
+export const login = user => async dispatch => {
+    const response = await apiUtil.login(user);
+    const data = await response.json();
+    
+    if (response.ok) {
+        return dispatch(receiveCurrentUser(data));
+    }
+    // todo: handle errors
+};
+
+export const signup = user => async dispatch => {
+    const response = await apiUtil.signup(user);
+    const data = await response.json();
+
+    if (response.ok) {
+        return dispatch(receiveCurrentUser(data));
+    }
+    // todo: handle errors
+};
+
+export const logout = () => async dispatch => {
+    const response = await apiUtil.logout();
+    const data = await response.json();
+
+    if (response.ok) {
+        return dispatch(logoutCurrentUser());
+    }
+    // todo:handle errors
+};
